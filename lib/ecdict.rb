ccb4e65af8bbb1e5a4f20d78016baca49fe9cc31
ccmywish <ccmywish@qq.com>
2022-04-29 23:38:23 +0800

    add header

--- lib/ecdict.rb
+++ lib/ecdict.rb
@@ -1,3 +1,17 @@
+# ---------------------------------------------------------------
+# File          : ecdict.rb
+# Authors       : ccmywish <ccmywish@qq.com>
+# Created on    : <2022-04-29>
+# Last modified : <2022-04-29>
+#
+# ecdict:
+#
+#   ecdict lib
+#
+# ---------------------------------------------------------------
+
 module ECDict
+  
   VERSION = "1.3.0"
+
 end
